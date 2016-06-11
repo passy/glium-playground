d00954b9c058cca9c30b8eb28b92a52e1c1a4751
Pascal Hartig <phartig@rdrei.net>
2016-06-11 14:16:16 +0100

    Clear it with a lovely pink

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ extern crate glium;
 
 fn main() {
     use glium::DisplayBuild;
+    use glium::Surface;
 
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    let mut target = display.draw();
+    target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
+    target.finish().unwrap();
 
     loop {
         for ev in display.poll_events() {
