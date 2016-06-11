84bed8c2b90f556c2b9e45e8ac9e18f1665d3d55
Pascal Hartig <phartig@rdrei.net>
2016-06-11 14:17:25 +0100

    Not necessary, but it's technically part of the rendering loop

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ fn main() {
 
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
-    let mut target = display.draw();
-    target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
-    target.finish().unwrap();
-
     loop {
+        let mut target = display.draw();
+        target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
+        target.finish().unwrap();
+
         for ev in display.poll_events() {
             match ev {
                 glium::glutin::Event::Closed => return,
