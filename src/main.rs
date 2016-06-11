845dfc5dc1c8b44b6bb705277fe8a7dee6562bd1
Pascal Hartig <phartig@rdrei.net>
2016-06-11 14:10:21 +0100

    A window that stays open ...
    ... and keeps your flat warm. Don't let this running for too long.

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,13 @@ fn main() {
     use glium::DisplayBuild;
 
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    loop {
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => (),
+            }
+        }
+    }
 }
