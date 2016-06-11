a29e53b80e0feb853435ff1d9484acb6d048e3d7
Pascal Hartig <phartig@rdrei.net>
2016-06-11 17:26:48 +0100

    Behold! A triangle!

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 
 extern crate glium;
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
 fn main() {
     use glium::DisplayBuild;
     use glium::Surface;
@@ -9,8 +16,47 @@ fn main() {
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
     loop {
+        let vertex1 = Vertex { position: [-0.5, -0.5] };
+        let vertex2 = Vertex { position: [0.5, -0.25] };
+        let vertex3 = Vertex { position: [0.0, 0.5] };
+        let shape = vec![vertex1, vertex2, vertex3];
+
+        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+
+        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+        let vertex_shader_src = r#"
+            #version 140
+
+            in vec2 position;
+
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 140
+
+            out vec4 color;
+
+            void main() {
+                color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        "#;
+
+        let program =
+            glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+                .unwrap();
+
         let mut target = display.draw();
         target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
+        target.draw(&vertex_buffer,
+                  &indices,
+                  &program,
+                  &glium::uniforms::EmptyUniforms,
+                  &Default::default())
+            .unwrap();
         target.finish().unwrap();
 
         for ev in display.poll_events() {
