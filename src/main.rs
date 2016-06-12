5fbf00bac7eb1df9583d51315196b9ff4b8211f3
Pascal Hartig <phartig@rdrei.net>
2016-06-12 12:17:57 +0100

    Move draw() to a separate function

--- src/main.rs
+++ src/main.rs
@@ -11,53 +11,12 @@ implement_vertex!(Vertex, position);
 
 fn main() {
     use glium::DisplayBuild;
-    use glium::Surface;
 
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
     loop {
-        let vertex1 = Vertex { position: [-0.5, -0.5] };
-        let vertex2 = Vertex { position: [0.5, -0.25] };
-        let vertex3 = Vertex { position: [0.0, 0.5] };
-        let shape = vec![vertex1, vertex2, vertex3];
-
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-
-        let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-
-        let vertex_shader_src = r#"
-            #version 140
-
-            in vec2 position;
-
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
-
-        let fragment_shader_src = r#"
-            #version 140
-
-            out vec4 color;
-
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
-
-        let program =
-            glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
-                .unwrap();
-
-        let mut target = display.draw();
-        target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
-        target.draw(&vertex_buffer,
-                  &indices,
-                  &program,
-                  &glium::uniforms::EmptyUniforms,
-                  &Default::default())
-            .unwrap();
-        target.finish().unwrap();
+        let d = draw(&display);
+        d.unwrap();
 
         for ev in display.poll_events() {
             match ev {
@@ -66,4 +25,53 @@ fn main() {
             }
         }
     }
+}
+
+fn draw(display: &glium::backend::glutin_backend::GlutinFacade)
+        -> Result<(), glium::SwapBuffersError> {
+    use glium::Surface;
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.5, -0.25] };
+    let vertex3 = Vertex { position: [0.0, 0.5] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(display, &shape).unwrap();
+
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program =
+        glium::Program::from_source(display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+
+    let mut target = display.draw();
+    target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
+    target.draw(&vertex_buffer,
+              &indices,
+              &program,
+              &glium::uniforms::EmptyUniforms,
+              &Default::default())
+        .unwrap();
+
+    target.finish()
 }
