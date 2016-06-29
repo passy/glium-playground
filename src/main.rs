dab809340019ce2aacafdd23831b8a7a7ab56d93
Pascal Hartig <phartig@rdrei.net>
2016-06-29 21:55:32 +0100

    Step one to teapot

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 extern crate glium;
 extern crate image;
+mod teapot;
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -19,36 +20,18 @@ fn main() {
     use glium::DisplayBuild;
     use std::io::Cursor;
 
-    let image = image::load(Cursor::new(&include_bytes!("../images/bird.png")[..]),
-                            image::PNG)
-        .unwrap()
-        .to_rgba();
-    let image_dimens = image.dimensions();
-    let image_texture = glium::texture::RawImage2d::from_raw_rgba_reversed(image.into_raw(),
-                                                                           image_dimens);
-
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
     let mut state = State { rot: -0.5 };
 
-    let vertex1 = Vertex {
-        position: [-0.5, -0.5],
-        tex_coords: [0.0, 0.0],
-    };
-    let vertex2 = Vertex {
-        position: [0.5, -0.25],
-        tex_coords: [0.0, 1.0],
-    };
-    let vertex3 = Vertex {
-        position: [0.0, 0.5],
-        tex_coords: [1.0, 0.0],
-    };
-    let shape = vec![vertex1, vertex2, vertex3];
-
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let texture = glium::texture::Texture2d::new(&display, image_texture).unwrap();
+    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display,
+                                          glium::index::PrimitiveType::TrianglesList,
+                                          &teapot::INDICES)
+        .unwrap();
 
     loop {
-        let (state_, d) = draw(state, &vertex_buffer, &display, &texture);
+        let (state_, d) = draw(state, &display, &positions, &normals, &indices);
         state = state_;
         d.unwrap();
 
@@ -61,48 +44,33 @@ fn main() {
 }
 
 fn draw(state: State,
-        vertex_buffer: &glium::VertexBuffer<Vertex>,
         display: &glium::backend::glutin_backend::GlutinFacade,
-        texture: &glium::texture::Texture2d)
+        positions: &glium::VertexBuffer<teapot::Vertex>,
+        normals: &glium::VertexBuffer<teapot::Normal>,
+        indices: &glium::IndexBuffer<u16>)
         -> (State, Result<(), glium::SwapBuffersError>) {
     use glium::Surface;
-
-    let mut rot = state.rot;
-    rot += 0.0002;
-
-    if rot > 0.5 {
-        rot = -0.5;
-    }
-
-    let state_ = State { rot: rot };
-
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
         #version 140
 
-        in vec2 position;
-        in vec2 tex_coords;
-        out vec2 v_tex_coords;
+        in vec3 position;
+        in vec3 normal;
 
         uniform mat4 matrix;
 
         void main() {
-            v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
+            gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
 
     let fragment_shader_src = r#"
         #version 140
 
-        in vec2 v_tex_coords;
         out vec4 color;
 
-        uniform sampler2D tex;
-
         void main() {
-            color = texture(tex, v_tex_coords);
+            color = vec4(1.0, 1.0, 0.0, 1.0);
         }
     "#;
 
@@ -114,19 +82,18 @@ fn draw(state: State,
             [1.0, 0.0, 0.0, 0.0],
             [0.0, 1.0, 0.0, 0.0],
             [0.0, 0.0, 1.0, 0.0],
-            [ rot , 0.0, 0.0, 1.0f32],
+            [0.0, 0.0, 0.0, 1.0f32],
         ],
-        tex: texture
     };
 
     let mut target = display.draw();
     target.clear_color(251.0 / 255.0, 34.0 / 255.0, 112.0 / 255.0, 1.0);
-    target.draw(vertex_buffer,
-              &indices,
+    target.draw((positions, normals),
+              indices,
               &program,
               &uniforms,
               &Default::default())
         .unwrap();
 
-    (state_, target.finish())
+    (state, target.finish())
 }
