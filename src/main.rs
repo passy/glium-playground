4e112c8bd43b5e872cf86e694104eaf1e35fe39a
Pascal Hartig <phartig@rdrei.net>
2016-07-01 22:33:48 +0100

    You show the lights that stop me turn to stooooone

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,12 @@ fn draw(state: State,
         in vec3 position;
         in vec3 normal;
 
+        out vec3 v_normal;
+
         uniform mat4 matrix;
 
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
@@ -67,10 +70,15 @@ fn draw(state: State,
     let fragment_shader_src = r#"
         #version 140
 
+        in vec3 v_normal;
         out vec4 color;
+        uniform vec3 u_light;
 
         void main() {
-            color = vec4(1.0, 1.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
@@ -84,6 +92,7 @@ fn draw(state: State,
             [0.0, 0.0, 0.01, 0.0],
             [0.0, 0.0, 0.0, 1.0f32],
         ],
+        u_light: [-1.0, 0.4, 0.9f32]
     };
 
     let mut target = display.draw();
